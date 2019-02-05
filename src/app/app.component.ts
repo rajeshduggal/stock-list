363dd473c613fe58027d92af6d790e00f7b6cc47
Rajesh Duggal <rajesh@duggalmail.com>
2019-02-05 02:19:22 -0500

    Add products table to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stock-list';
+
+  products = [];
+  ngOnInit() {
+    this.products = this.getProducts();
+  }
+  getProducts() {
+    return [
+      { 'id': '1', 'title': 'Screw Driver', 'price': 400, 'stock': 11 },
+      { 'id': '2', 'title': 'Nut Volt', 'price': 200, 'stock': 5 },
+      { 'id': '3', 'title': 'Resistor', 'price': 78, 'stock': 45 },
+      { 'id': '4', 'title': 'Tractor', 'price': 20000, 'stock': 1 },
+      { 'id': '5', 'title': 'Roller', 'price': 62, 'stock': 15 }
+    ];
+  }
+
 }
